4fe69adc0d8fb21a447edd999fe617fb0853c36b
nilsandrey <nils@developguide.com>
2021-06-23 13:24:13 -0400

    Added main page

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   resources :accounts do
     get 'status', on: :member
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "info#index"
 end
